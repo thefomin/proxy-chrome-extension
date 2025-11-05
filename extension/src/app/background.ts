d10b660b236cdda53d2fb7ff3ab325a3fcfd4cc9
thefomin <raveyt@yandex.ru>
2025-11-05 21:39:55 +0700

    feat: added chrome listener on background

--- extension/src/app/background.ts
+++ extension/src/app/background.ts
@@ -1,1 +1,22 @@
+import { proxyConfig, ProxyMessage } from "@/shared/config/proxy";
+
 console.log("service_worker working...");
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === ProxyMessage.CONNECT) {
+    chrome.proxy.settings.set(proxyConfig, async () => {
+      console.log("PROXY ПОДКЛЮЧЕН");
+      await chrome.storage.local.set({ proxyEnabled: true });
+      sendResponse({ success: true });
+    });
+    return true;
+  }
+  if (message.type === ProxyMessage.DISCONNECT) {
+    chrome.proxy.settings.clear({ scope: "regular" }, async () => {
+      console.log("Proxy отключен!");
+      await chrome.storage.local.set({ proxyEnabled: false });
+      sendResponse({ success: true });
+    });
+    return true;
+  }
+});
