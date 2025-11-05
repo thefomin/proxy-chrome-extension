7b4a7419ab533f99fd810daac756c77eaca65f80
thefomin <raveyt@yandex.ru>
2025-11-05 21:43:38 +0700

    chore(manifest): added permissions

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -1,32 +1,37 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    48: "public/logo.png",
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      48: "public/logo.png",
     },
-    default_popup: 'src/app/index.html',
+    default_popup: "src/app/index.html",
   },
   permissions: [
-    'sidePanel',
-    'contentSettings',
+    "sidePanel",
+    "contentSettings",
+    "proxy",
+    "storage",
+    "scripting",
   ],
-  content_scripts: [{
-    js: ['src/app/content/main.tsx'],
-    matches: ['https://*/*'],
-  }],
+  content_scripts: [
+    {
+      js: ["src/app/content/main.tsx"],
+      matches: ["https://*/*"],
+    },
+  ],
   side_panel: {
-    default_path: 'src/app/sidepanel/index.html',
+    default_path: "src/app/sidepanel/index.html",
   },
-  background:{
-    service_worker: 'src/app/background.ts',
-    type: "module"
-  }
-})
+  background: {
+    service_worker: "src/app/background.ts",
+    type: "module",
+  },
+});
