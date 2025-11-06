b41dc98320cf4d7d529c0945d7adb33e2ff07cb7
thefomin <raveyt@yandex.ru>
2025-11-06 21:52:36 +0700

    refactor: added proxy schema from chrome

--- extension/src/shared/config/proxy.ts
+++ extension/src/shared/config/proxy.ts
@@ -4,23 +4,8 @@ export const StorageArea = {
   LOCAL: "local" as const,
   SYNC: "sync" as const,
 };
-
+export type ProxySchema = "http" | "https" | "quic" | "socks4" | "socks5";
 export type StorageAreaType = (typeof StorageArea)[keyof typeof StorageArea];
-
-export const proxyConfig: chrome.types.ChromeSettingSetDetails<chrome.proxy.ProxyConfig> =
-  {
-    value: {
-      mode: "fixed_servers",
-      rules: {
-        singleProxy: {
-          scheme: "socks5",
-          host: "74.119.147.209",
-          port: 4145,
-        },
-      },
-    },
-    scope: "regular",
-  };
 
 export enum ProxyMessage {
   CONNECT = "CONNECT_PROXY",
