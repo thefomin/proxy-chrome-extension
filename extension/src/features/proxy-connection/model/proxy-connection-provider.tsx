249ec9c51496628f869dd38770ffb6437ddc8e24
thefomin <raveyt@yandex.ru>
2025-11-06 21:54:56 +0700

    feat: added proxy connection provider

---
+++ extension/src/features/proxy-connection/model/proxy-connection-provider.tsx
@@ -0,0 +1,113 @@
+import { createContext, useContext } from "react";
+import {
+  STORAGE_KEY,
+  StorageArea,
+  ProxyMessage,
+  ProxySchema,
+} from "@/shared/config/proxy";
+import { useStorage } from "@/shared/model/use-storage";
+import { useProxyById } from "./use-proxy-by-id";
+import { ApiSchemas } from "@/shared/api/schema";
+import { useSearchParams } from "react-router-dom";
+
+interface ProxyConnectionContextValue {
+  isConnected: boolean;
+  connectProxy: () => Promise<void>;
+  disconnectProxy: () => Promise<void>;
+  proxy?: ApiSchemas["Proxy"];
+  isPending: boolean;
+  errorMessage?: string;
+  setProxyParamsId: (id: string) => void;
+  proxyParamsId: string;
+}
+
+const ProxyConnectionContext =
+  createContext<ProxyConnectionContextValue | null>(null);
+
+export const ProxyConnectionProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isConnected, setIsConnected] = useStorage<boolean>(
+    STORAGE_KEY,
+    false,
+    StorageArea.LOCAL
+  );
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  //сюда нужно прикрутить получения данных через params и initialProxyId
+  const proxyParamsId =
+    searchParams.get("proxyId") || "9fb8dd11-094d-4c9d-9a58-acca2aa11773";
+
+  const { proxy, isPending, errorMessage } = useProxyById(proxyParamsId);
+
+  const setProxyParamsId = (id: string) => {
+    setSearchParams({ proxyId: id });
+  };
+
+  const connectProxy = async () => {
+    if (!proxy?.ip || !proxy?.port || !proxy?.protocol) return;
+
+    const config: chrome.types.ChromeSettingSetDetails<chrome.proxy.ProxyConfig> =
+      {
+        value: {
+          mode: "fixed_servers",
+          rules: {
+            singleProxy: {
+              scheme: proxy.protocol as ProxySchema,
+              host: proxy.ip,
+              port: proxy.port,
+            },
+          },
+        },
+        scope: "regular",
+      };
+    try {
+      chrome.runtime.sendMessage(
+        { type: ProxyMessage.CONNECT, config },
+        (res) => {
+          console.log("proxy " + JSON.stringify(proxy));
+        }
+      );
+      setIsConnected(true);
+    } catch (e) {
+      console.error("Ошибка подключения", e);
+    }
+  };
+
+  const disconnectProxy = async () => {
+    try {
+      await chrome.runtime.sendMessage({ type: ProxyMessage.DISCONNECT });
+      setIsConnected(false);
+    } catch (e) {
+      console.error("Ошибка отключения", e);
+    }
+  };
+
+  return (
+    <ProxyConnectionContext.Provider
+      value={{
+        isConnected,
+        connectProxy,
+        disconnectProxy,
+        proxy,
+        isPending,
+        errorMessage,
+        setProxyParamsId,
+        proxyParamsId,
+      }}
+    >
+      {children}
+    </ProxyConnectionContext.Provider>
+  );
+};
+
+export const useProxyConnection = () => {
+  const context = useContext(ProxyConnectionContext);
+  if (!context)
+    throw new Error(
+      "useProxyConnection должен использоваться внутри ProxyConnectionProvider"
+    );
+  return context;
+};
