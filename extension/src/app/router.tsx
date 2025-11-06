1f4de917003c54b3ca832cde5004db3d5594a6b6
thefomin <raveyt@yandex.ru>
2025-11-06 21:54:13 +0700

    chore(router): added proxy list page

--- extension/src/app/router.tsx
+++ extension/src/app/router.tsx
@@ -3,8 +3,7 @@ import App from "./app";
 import { ROUTES } from "@/shared/config/routes";
 import { ProtectedRoute } from "./protected-route";
 import { Providers } from "./providers";
-
-// используем createHashRouter потому что расширения не имеют серверного роутинга
+//используем createHashRouter потому что расширения не имеют серверного роутинга
 //вместо localhost
 export const router = createHashRouter([
   {
@@ -19,7 +18,7 @@ export const router = createHashRouter([
         children: [
           {
             path: ROUTES.CONNECTION,
-            lazy: () => import("@/pages/connection.page"),
+            lazy: () => import("@/pages/proxy-connection.page"),
           },
         ],
       },
@@ -30,6 +29,10 @@ export const router = createHashRouter([
       {
         path: ROUTES.SUGN_UP,
         lazy: () => import("@/pages/auth/sign-up.page"),
+      },
+      {
+        path: ROUTES.PROXY_LIST,
+        lazy: () => import("@/pages/proxy-list.page"),
       },
     ],
   },
