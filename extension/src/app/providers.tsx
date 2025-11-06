9bd09483a7390d2ede84cbb892a2da6c3ce9a6bb
thefomin <raveyt@yandex.ru>
2025-11-06 21:53:46 +0700

    reafactor: changed import provider

--- extension/src/app/providers.tsx
+++ extension/src/app/providers.tsx
@@ -1,4 +1,4 @@
-import { ProxyConnectionProvider } from "@/shared/provider/proxy-connection-provider";
+import { ProxyConnectionProvider } from "@/features/proxy-connection/model/proxy-connection-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ProxyConnectionProvider>{children}</ProxyConnectionProvider>;
