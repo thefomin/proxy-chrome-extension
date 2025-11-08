13cd5831a2e4855c40aaeb3680f8a0c7b7eb13b5
thefomin <raveyt@yandex.ru>
2025-11-08 16:24:32 +0100

    feat: added tanstack query-client

--- extension/src/app/providers.tsx
+++ extension/src/app/providers.tsx
@@ -1,5 +1,11 @@
 import { ProxyConnectionProvider } from "@/features/proxy-connection/model/proxy-connection-provider";
+import { queryClient } from "@/shared/api/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ProxyConnectionProvider>{children}</ProxyConnectionProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProxyConnectionProvider>{children}</ProxyConnectionProvider>
+    </QueryClientProvider>
+  );
 }
