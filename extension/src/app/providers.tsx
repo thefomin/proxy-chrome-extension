b9cc0cb8b3efc9c865cc862bb242160b67b916f0
thefomin <raveyt@yandex.ru>
2025-11-10 03:42:06 +0100

    chore: added session provider

--- extension/src/app/providers.tsx
+++ extension/src/app/providers.tsx
@@ -1,11 +1,14 @@
 import { ProxyConnectionProvider } from "@/features/proxy-connection/model/proxy-connection-provider";
 import { queryClient } from "@/shared/api/query-client";
+import { SessionProvider } from "@/shared/model/session-provider";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ProxyConnectionProvider>{children}</ProxyConnectionProvider>
+      <SessionProvider>
+        <ProxyConnectionProvider>{children}</ProxyConnectionProvider>
+      </SessionProvider>
     </QueryClientProvider>
   );
 }
