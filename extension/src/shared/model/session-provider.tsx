5910630ed86b1a6c32d01650ecd68b2f7251fcc5
thefomin <raveyt@yandex.ru>
2025-11-10 03:38:51 +0100

    feat: added session provider

---
+++ extension/src/shared/model/session-provider.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useStorage } from "@/shared/model/use-storage";
+import { StorageArea } from "../config/chrome-storage";
+
+export type Session = {
+  authId: string;
+} | null;
+
+type SessionContextType = {
+  session: Session;
+  login: (data: Session) => void;
+  logout: () => void;
+};
+
+const STORAGE_KEY = 'session'
+
+const SessionContext = createContext<SessionContextType | null>(null);
+
+export const SessionProvider = ({ children }: { children: React.ReactNode }) => {
+  const [session, setSession] = useStorage<Session>(STORAGE_KEY, null, StorageArea.LOCAL);
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    chrome.runtime.sendMessage({ type: "GET_SESSION" }, (response) => {
+      if (response?.session) setSession(response.session);
+      setIsLoading(false);
+    });
+  },[])
+
+  const login = (data: Session) => {
+    chrome.runtime.sendMessage({ type: "SET_SESSION", session: data });
+    setSession(data);
+  };
+
+  const logout = () => {
+    chrome.runtime.sendMessage({ type: "CLEAR_SESSION" });
+    setSession(null);
+  };
+
+  if (isLoading) return null;
+
+  return (
+    <SessionContext.Provider value={{ session, login, logout }}>
+      {children}
+    </SessionContext.Provider>
+  );
+};
+
+export const useSession = () => {
+  const ctx = useContext(SessionContext);
+  if (!ctx) throw new Error("useSession must be used inside SessionProvider");
+  return ctx;
+};
