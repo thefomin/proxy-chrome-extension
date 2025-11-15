0ea4214a306306243bd8d013730623371cb561a6
thefomin <raveyt@yandex.ru>
2025-11-15 18:06:13 +0100

    feat: added svg component

---
+++ extension/src/features/auth/ui/svg-icon.tsx
@@ -0,0 +1,100 @@
+const CircleSvg = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={390}
+    height={402}
+    viewBox="0 0 390 402"
+    fill="none"
+    {...props}
+  >
+    <path
+      d="M55.9467 50.2775C25.162 84.6299 5.4181 130.343 3.13374 181.21C-1.91072 293.537 80.0947 388.462 186.298 393.232C292.501 398.001 382.685 310.809 387.73 198.481C391.279 119.444 351.73 49.0218 290.978 12.5612"
+      stroke="url(#paint0_linear_145_82)"
+      strokeWidth={1.71104}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M289.375 232.27C301.11 208.389 303.787 180.049 294.628 152.838C276.216 98.1323 216.943 68.7106 162.237 87.1226C107.532 105.535 78.1098 164.808 96.5219 219.514C109.019 256.644 140.339 282.126 176.278 288.911"
+      stroke="white"
+      strokeWidth={1.71104}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M289.375 232.27C301.11 208.389 303.787 180.049 294.628 152.838C276.216 98.1323 216.943 68.7106 162.237 87.1226C107.532 105.535 78.1098 164.808 96.5219 219.514C109.019 256.644 140.339 282.126 176.278 288.911"
+      stroke="url(#paint1_linear_145_82)"
+      strokeWidth={1.71104}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M109.243 313.962C141.783 335.628 182.554 344.951 223.944 336.818C307.156 320.469 361.359 239.757 345.01 156.545C328.66 73.3325 247.949 19.1295 164.736 35.4793C108.258 46.5762 65.1435 87.3222 48.3817 138.376"
+      stroke="white"
+      strokeWidth={2.51385}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M109.243 313.962C141.783 335.628 182.554 344.951 223.944 336.818C307.156 320.469 361.359 239.757 345.01 156.545C328.66 73.3325 247.949 19.1295 164.736 35.4793C108.258 46.5762 65.1435 87.3222 48.3817 138.376"
+      stroke="url(#paint2_linear_145_82)"
+      strokeWidth={2.51385}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M146.203 151.902C138.776 164.083 135.969 179.085 139.536 194.06C146.708 224.166 176.927 242.757 207.033 235.586C237.138 228.414 255.73 198.195 248.559 168.089C243.691 147.656 228.206 132.526 209.346 127.156"
+      stroke="white"
+      strokeWidth={1.71104}
+      strokeDasharray="8.56 8.56"
+    />
+    <path
+      d="M146.203 151.902C138.776 164.083 135.969 179.085 139.536 194.06C146.708 224.166 176.927 242.757 207.033 235.586C237.138 228.414 255.73 198.195 248.559 168.089C243.691 147.656 228.206 132.526 209.346 127.156"
+      stroke="url(#paint3_linear_145_82)"
+      strokeWidth={1.71104}
+      strokeDasharray="8.56 8.56"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_145_82"
+        x1={238.479}
+        y1={89.4189}
+        x2={56.8963}
+        y2={175.894}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#2F322F" />
+        <stop offset={1} stopColor="#BF51FF" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_145_82"
+        x1={192.026}
+        y1={241.161}
+        x2={263.666}
+        y2={160.954}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#2F322F" />
+        <stop offset={1} stopColor="#BF51FF" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear_145_82"
+        x1={114.924}
+        y1={170.455}
+        x2={218.048}
+        y2={290.163}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#2F322F" />
+        <stop offset={1} stopColor="#BF51FF" />
+      </linearGradient>
+      <linearGradient
+        id="paint3_linear_145_82"
+        x1={198.616}
+        y1={151.888}
+        x2={156.463}
+        y2={191.232}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#2F322F" />
+        <stop offset={1} stopColor="#BF51FF" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default CircleSvg;
