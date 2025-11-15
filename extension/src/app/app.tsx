48dfff1cfa01b10ab9b17f3dfa0eb4c889905313
thefomin <raveyt@yandex.ru>
2025-11-15 18:05:05 +0100

    chore: update style app

--- extension/src/app/app.tsx
+++ extension/src/app/app.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 export default function App() {
   usePersistRoute();
   return (
-    <main className="w-screen">
+    <main className="w-max-[360px] h-screen">
       <Outlet />
     </main>
   );
