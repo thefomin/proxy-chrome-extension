9405f2a7662b26c1d1a1512cfe359da284933fd7
thefomin <raveyt@yandex.ru>
2025-11-15 18:07:04 +0100

    chore: update const auth

---
+++ extension/src/features/auth/model/use-login.ts
@@ -0,0 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ApiSchemas } from '@/shared/api/schema';
+import { publicRqClient } from '@/shared/api/instance.api';
+import { ROUTES } from '@/shared/config/routes';
+import { useSession } from '@/shared/model/session-provider';
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const session = useSession();
+
+  const loginMutation = publicRqClient.useMutation('post', '/auth/login', {
+    onSuccess(data: ApiSchemas['Session']) {
+      session.login({
+        user: {
+          authId: data.user.authId,
+        },
+        proxyId: data.proxyId!,
+      });
+
+      console.log('login data ' + JSON.stringify(data));
+      navigate(ROUTES.CONNECTION);
+    },
+  });
+
+  const login = (data: ApiSchemas['AuthRequest']) => {
+    loginMutation.mutate({ body: data });
+  };
+
+  const errorMessage = loginMutation.isError ? loginMutation.error : undefined;
+
+  return {
+    login,
+    isPending: loginMutation.isPending,
+    errorMessage,
+  };
+}
