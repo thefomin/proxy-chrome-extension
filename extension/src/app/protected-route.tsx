0069e4ceea8f426f0ab9f72c18ea93ff73fe3bbe
thefomin <raveyt@yandex.ru>
2025-11-16 00:13:21 +0700

    fix: change navigate path

--- extension/src/app/protected-route.tsx
+++ extension/src/app/protected-route.tsx
@@ -1,13 +1,13 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate } from 'react-router-dom';
 
-import { ROUTES } from "@/shared/config/routes";
-import { useSession } from "@/shared/model/session-provider";
+import { ROUTES } from '@/shared/config/routes';
+import { useSession } from '@/shared/model/session-provider';
 
 export function ProtectedRoute() {
-  const {session} = useSession()
-  
+  const { session } = useSession();
+
   if (!session) {
-    return <Navigate to={ROUTES.SIGN_IN} />;
+    return <Navigate to={ROUTES.AUTH} />;
   }
 
   return <Outlet />;
